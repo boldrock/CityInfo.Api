d507494a833c31e99f424f43e9e7d07d137d1b0f
Joe Smith <joe.s@carevisions.co.uk>
2021-02-21 14:02:19 +0000

    Add project files.

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,36 @@
+﻿using CityInfo.API;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            var cities = CitiesDataStore.Current.Cities;
+           
+            return Ok(cities);
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+          
+        }
+    }
+}
